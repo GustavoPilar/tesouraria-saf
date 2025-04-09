8b359fd39ff691c7fda0d35a07528ad6f76333d1
Gustavo Pilar <pilargustavo7@gmail.com>
2025-04-08 21:43:11 -0300

    Build para GitHub Pages na pasta docs

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+        by
+        <a href="https://gustavopilar.github.com" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Gustavo Pilar</a>
     </div>`
 })
 export class AppFooter {}
